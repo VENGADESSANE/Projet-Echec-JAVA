aa5af3139b19c89c54f5f62844845e1e49310fab
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-11 09:30:47 +0200

    Update Pion_noir.java

--- Pion_noir.java
+++ Pion_noir.java
@@ -1,7 +1,14 @@
 public class Pion_noir extends Piece {
+	private int compteur;
 	
 	public Pion_noir() {
 		super("♟");
 	}
 	
+	public boolean verif(Case depart, Case arrive) {
+		if (compteur == 1 && depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {return true;}
+		else if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {return true;}
+		else {return false;}
+		
+	}
 }
