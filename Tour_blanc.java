5a664d5fb594de9346726c31b22b372510bb951f
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-12 21:05:52 +0200

    Update Tour_blanc.java

--- Tour_blanc.java
+++ Tour_blanc.java
@@ -9,4 +9,39 @@ public class Tour_blanc extends Piece {
 		else if (depart.getX() == depart.getX()) {return true;}
 		else {return false;}
 	}
+	
+	// Vérification si personne est sur le chemin
+	public boolean verif_case_libre(Case depart,Case arrive) {
+		// si le mouvement s'effectue en horizontale 
+		if (depart.getY() == arrive.getY() && depart.getX() != arrive.getX()) {
+			// si le mouvement s'effectue vers la droite
+			if (arrive.getX() > depart.getX()) {
+				for (int i = depart.getX()+1; i < arrive.getX(); i++) {
+					if (this.echiquier.verif_case_vide(i, depart.getY()) == false) {return false;}
+				}
+			}
+			// si le mouvement s'effectue vers la gauche
+			if (arrive.getX() < depart.getX()) {
+				for (int i = depart.getX()-1; i > arrive.getX(); i--) {
+					if (this.echiquier.verif_case_vide(i, depart.getY()) == false) {return false;}
+				}
+			}
+		}
+		// si le mouvement s'effectue en vertical
+		if (depart.getY() != arrive.getY() && depart.getX() == arrive.getX()) {
+			// si le mouvement s'effectue vers le haut
+			if (arrive.getY() > depart.getY()) {
+				for (int i = depart.getY()+1; i < arrive.getY(); i++) {
+					if (this.echiquier.verif_case_vide(i, depart.getY()) == false) {return false;}
+				}
+			}
+			// si le mouvement s'effectue vers le bas
+			if (arrive.getY() < depart.getY()) {
+				for (int i = depart.getY()-1; i > arrive.getY(); i--) {
+					if (this.echiquier.verif_case_vide(i, depart.getY()) == false) {return false;}
+				}
+			}
+		}
+		return true;
+	}
 }
