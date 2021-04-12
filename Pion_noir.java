924d27b3fb921d84d0d95f0cd756cea473b34c36
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-12 21:32:35 +0200

    Update Pion_noir.java

--- Pion_noir.java
+++ Pion_noir.java
@@ -1,14 +1,27 @@
 public class Pion_noir extends Piece {
+	private Echiquier echiquier;
 	private int compteur;
 	
-	public Pion_noir() {
+	public Pion_noir(Echiquier echiquier) {
 		super("♟");
+		this.echiquier = echiquier;
 	}
 	
 	public boolean verif(Case depart, Case arrive) {
 		if (compteur == 1 && depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {return true;}
 		else if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {return true;}
-		else {return false;}
-		
+		else {return false;}	
 	}
+	
+	public boolean verif_case_libre(Case depart, Case arrive) {
+		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {
+			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-1) == false) {return false;}
+			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-2) == false) {return false;}
+		}
+		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {
+			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-1) == false) {return false;}
+		}
+		return true;
+	}
+		
 }
