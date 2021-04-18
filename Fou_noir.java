4e01de23a347bce0284c2ee262abe8430b4d7b6b
satia05 <62488137+satia05@users.noreply.github.com>
2021-04-18 15:52:04 +0200

    Update Fou_noir.java

--- Fou_noir.java
+++ Fou_noir.java
@@ -18,5 +18,65 @@ public class Fou_noir extends Piece {
 		return false;
 	}
 	
+	public boolean verif_case_libre(Case depart, Case arrive) {
+		
+		if(depart.getY() != arrive.getY() && depart.getX() != arrive.getX()) {
+			
+			//si le mouvement s'effectue vers en haur à droite
+			if(arrive.getX() > depart.getX() && arrive.getY() > depart.getY()) {
+				int px = arrive.getX();
+				int py = arrive.getY();
+				while(depart.getX() < px && depart.getY() < px) {
+					if(this.echiquier.verif_case_vide(px, py) == false) {
+						return false;
+						px -= 1;
+						py -= 1;
+					}
+					return true;
+				}
+			}
+			//si le mouvement s'effectue vers en bas à gauche
+			if(arrive.getX() > depart.getX() && arrive.getY() > depart.getY()) {
+				int px = arrive.getX();
+				int py = arrive.getY();
+				while(depart.getX() < px && depart.getY() < px) {
+					if(this.echiquier.verif_case_vide(px, py) == false) {
+						return false;
+						px += 1;
+						py += 1;
+					}
+					return true;
+				}
+			}
+			//si le mouvement s'effectue vers en haut à gauche
+			if(arrive.getX() > depart.getX() && arrive.getY() > depart.getY()) {
+				int px = arrive.getX();
+				int py = arrive.getY();
+				while(depart.getX() < px && depart.getY() < px) {
+					if(this.echiquier.verif_case_vide(px, py) == false) {
+						return false;
+						px += 1;
+						py -= 1;
+					}
+					return true;
+				}
+			}
+			//si le mouvement s'effectue vers en bas à droite
+			if(arrive.getX() > depart.getX() && arrive.getY() > depart.getY()) {
+				int px = arrive.getX();
+				int py = arrive.getY();
+				while(depart.getX() < px && depart.getY() < px) {
+					if(this.echiquier.verif_case_vide(px, py) == false) {
+						return false;
+						px -= 1;
+						py += 1;
+					}
+					return true;
+				}
+			}
+		}
+			
+	}
+	
 }
 
