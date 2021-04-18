3ea193861cf8babb2b6b66cf91941868719fe081
satia05 <62488137+satia05@users.noreply.github.com>
2021-04-18 15:55:45 +0200

    Update Cavalier_blanc.java

--- Cavalier_blanc.java
+++ Cavalier_blanc.java
@@ -4,5 +4,61 @@ public class Cavalier_blanc extends Piece {
 		super("♘");
 	}
 	
+	public boolean devant_droite(Case depart, Case arrive) {
+			if(arrive.getX() == depart.getX()- 1 && arrive.getY() == depart.getY() - 2)
+				return true;
+			else 
+				return false;
+	}
+	
+	public boolean devant_gauche(Case depart, Case arrive) {
+			if(arrive.getX() == depart.getX() + 1 && arrive.getY() == depart.getY() - 2)
+				return true;
+			else 
+				return false;
+	}
+	
+	public boolean droite_haut(Case depart, Case arrive) {
+		if(arrive.getX() == depart.getX() - 2 && arrive.getY() == depart.getY() - 1)
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean droite_bas(Case depart, Case arrive) {
+		if(arrive.getX() == depart.getX() - 2 && arrive.getY() == depart.getY() + 1)
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean gauche_haut(Case depart, Case arrive) {
+		if(arrive.getX() == depart.getX() + 2 && arrive.getY() == depart.getY() - 1)
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean gauche_bas(Case depart, Case arrive) {
+		if(arrive.getX() == depart.getX() + 2 && arrive.getY() == depart.getY() + 1)
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean derriere_droite(Case depart, Case arrive) {
+		if(arrive.getX() == depart.getX()- 1 && arrive.getY() == depart.getY() + 2)
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean derriere_gauche(Case depart, Case arrive) {
+		if(arrive.getX() == depart.getX()+ 1 && arrive.getY() == depart.getY() + 2)
+			return true;
+		else 
+			return false;
+	}
+	
 }
 
