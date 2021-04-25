9e40c1e57f2dddae82d72f700df186889f9df4b9
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-25 10:31:57 +0200

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -5,6 +5,7 @@ public class Jeu {
 	private Echiquier echiquier;
 	private Player joueur1;
 	private Player joueur2;
+	private int compteur;
 	
 public Jeu() {
 	System.out.println("Entrer le nom du premier joueur");
@@ -20,34 +21,58 @@ public Jeu() {
 	System.out.println(nom1 + " vous jouez les piéces blanches.");
 	System.out.println("Et "+ nom2 + " vous jouez les piéces noires.");
 	
-	this.echiquier = new Echiquier();
+	this.echiquier = new Echiquier(this);
 	}
 	
 public void jeu_en_cours() {
 
-	int i = 0;
-	while ( i == 0 ) { 
+	this.compteur = 1;
+	while ( this.compteur > 0 ) { 
 		this.echiquier.affiche();
-		System.out.println("Indiquez les coordonnées de la case de la piéce que vous voulez jouez ?");
+		
+		if ( this.compteur%2 == 0 ) {
+			System.out.println("* Au tour des noires *");
+		}
+		else { System.out.println("* Au tour des blancs *"); }
+		
+		System.out.println("** Indiquez les coordonnées de la case de la piéce que vous voulez jouez ? **");
 		Scanner case1 = new Scanner(System.in);
 		String coordonees_d = case1.nextLine();
 		int[] tab_coordo1 = this.echiquier.conversion1(coordonees_d);
-		Case case_d = new Case( tab_coordo1[1], tab_coordo1[0] );
+		if ( tab_coordo1[1] >= 0 && tab_coordo1[1] <= 7 && tab_coordo1[0] >= 0 && tab_coordo1[0] <= 7) {
+			Case case_d = new Case( tab_coordo1[1], tab_coordo1[0] );
 		
-		System.out.println("Indiquez les coordonnées de la case sur la qu'elle vous voulez vous déplacer ?");
-		Scanner case2 = new Scanner(System.in);
-		String coordonees_a = case2.nextLine();
-		int[] tab_coordo2 = this.echiquier.conversion1(coordonees_a);
-		Case case_a = new Case( tab_coordo2[1], tab_coordo2[0] );
+			System.out.println("** Indiquez les coordonnées de la case sur la qu'elle vous voulez vous déplacer ? **");
+			Scanner case2 = new Scanner(System.in);
+			String coordonees_a = case2.nextLine();
+			int[] tab_coordo2 = this.echiquier.conversion1(coordonees_a);
+			if ( tab_coordo2[1] >= 0 && tab_coordo2[1] <= 7 && tab_coordo2[0] >= 0 && tab_coordo2[0] <= 7) {
+				Case case_a = new Case( tab_coordo2[1], tab_coordo2[0] );
 		
-		Piece piece_d = this.echiquier.tableau[ tab_coordo1[0] ][ tab_coordo1[1] ];
+				Piece piece_d = this.echiquier.tableau[ tab_coordo1[0] ][ tab_coordo1[1] ];
+				if ((this.compteur%2 == 0 && piece_d.getCouleur() == 1) || (this.compteur%2 != 0 && piece_d.getCouleur() == 0)) {
 		
-		if (piece_d.verif(case_d, case_a) == true && piece_d.verif_case_libre(case_d, case_a) == true) {
-			this.echiquier.tableau[ tab_coordo1[0] ][ tab_coordo1[1] ] = new Piece(" ");
-			this.echiquier.tableau[ tab_coordo2[0] ][ tab_coordo2[1] ] = piece_d;
+					if (piece_d.verif(case_d, case_a) == true && piece_d.verif_case_libre(case_d, case_a) == true) {
+						this.echiquier.tableau[ tab_coordo1[0] ][ tab_coordo1[1] ] = new Piece(" ",2);
+						this.echiquier.tableau[ tab_coordo2[0] ][ tab_coordo2[1] ] = piece_d;
+					}
+					else { System.out.println("-- Mouvement impossible -- "); }
+					this.compteur = this.compteur + 1;
+				}
+				else { System.out.println("-- Vous pouvez jouer uniquement vos piéces --"); }
+				
 			}
-		else { System.out.println("impossible"); }
-	}
+			else { System.out.println("-- Veuillez choisir une case de l'echiquier --"); }
+				
+		}
+		else { System.out.println("-- Veuillez jouer une piéce étant dans une case de l'echiquier --"); }
+	} 
+}
+	
+public boolean premierCoup() {
+	if (this.compteur == 1 || this.compteur == 2 ) {return true;}
+	else {return false;}
+}
 			
 		
 		
@@ -56,7 +81,7 @@ public void jeu_en_cours() {
 		
 		
 		
-}
+
 	
 
 
@@ -67,5 +92,4 @@ public static void main(String[] args) {
 	jeu.jeu_en_cours();
 }
 }
-
 
