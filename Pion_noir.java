e962110fb84244855157761d3579da75eaa3a15c
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-25 10:33:54 +0200

    Update Pion_noir.java

--- Pion_noir.java
+++ Pion_noir.java
@@ -1,27 +1,32 @@
 public class Pion_noir extends Piece {
 	private Echiquier echiquier;
-	private int compteur;
-	
+
 	public Pion_noir(Echiquier echiquier) {
-		super("♟");
+		super("♟",1);
 		this.echiquier = echiquier;
 	}
 	
 	public boolean verif(Case depart, Case arrive) {
-		if (compteur == 1 && depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {return true;}
-		else if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {return true;}
+		if (this.premierCoup() == true && depart.getX() == arrive.getX() && arrive.getY() == depart.getY()+2) {return true;}
+		else if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()+1) {return true;}
 		else {return false;}	
 	}
 	
 	public boolean verif_case_libre(Case depart, Case arrive) {
-		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {
-			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-1) == false) {return false;}
-			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-2) == false) {return false;}
+		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()+2) {
+			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()+1) == false) {return false;}
+			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()+2) == false) {return false;}
 		}
-		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {
-			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-1) == false) {return false;}
+		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()+1) {
+			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()+1) == false) {return false;}
 		}
 		return true;
 	}
 		
+
+	public boolean premierCoup() {
+		if ( this.echiquier.premierCoup() == true ) {return true;}
+		else {return false;}
+	}
+	
 }
