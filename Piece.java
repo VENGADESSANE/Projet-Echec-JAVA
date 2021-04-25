ff6f575b6450d6b9c3eb7f781c120ff6223351be
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-25 10:32:34 +0200

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,10 +1,11 @@
 
 public class Piece {
 	private String nom;
+	private int couleur;
 	
-	
-	public Piece(String symbole) {
+	public Piece(String symbole, int nCouleur) {
 		this.nom = symbole;
+		this.couleur = nCouleur;
 
 	}
 	
@@ -19,6 +20,10 @@ public class Piece {
 	public void setNom(String symbole) {
 		this.nom = symbole;
 	}
+	
+	public int getCouleur() {
+		return this.couleur;
+	}
 
 	/* FIN GETTERS & SETTERS */
 
@@ -31,6 +36,9 @@ public class Piece {
 		if ( arrive.getX() >= 0 && arrive.getX() <= 7 && arrive.getY() >= 0 && arrive.getY() <= 7) {return true;} 
 		else {return false;}
 	}
+	
+	public boolean verif(Case depart, Case arrive) { return false; }
+	public boolean verif_case_libre(Case depart, Case arrive) { return false; }
 	
 	public static void main(String args[]) {
 		
