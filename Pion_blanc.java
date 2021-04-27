3a21b35a2f84838ca6c99f7e9ab4b1917a909d12
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-27 20:40:56 +0200

    Update Pion_blanc.java

--- Pion_blanc.java
+++ Pion_blanc.java
@@ -1,14 +1,26 @@
 public class Pion_blanc extends Piece {
 	private Echiquier echiquier;
+	private boolean premierCoup;
 
 	public Pion_blanc(Echiquier echiquier) {
 		super("♙",0);
 		this.echiquier = echiquier;
+		this.premierCoup = true;
 	}
 	
 	public boolean verif(Case depart, Case arrive) {
-		if (this.premierCoup() == true && depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {return true;}
+	System.out.println("ok");
+		if (this.premierCoup == true && depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-2) {return true;}
 		else if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {return true;}
+		else if ((depart.getX()-1 == arrive.getX() && arrive.getY()-1 == depart.getY())
+			|| (depart.getX()+1 == arrive.getX() && arrive.getY()-1 == depart.getY())
+			|| (depart.getX()-1 == arrive.getX() && arrive.getY()+1 == depart.getY())
+			|| (depart.getX()+1 == arrive.getX() && arrive.getY()+1 == depart.getY())) {
+			System.out.println("ok");
+			if (this.echiquier.verif_case_vide(arrive.getX(), arrive.getY()) == false){
+				return true;}
+			else {return false;}
+			}	
 		else {return false;}
 	}
 	
@@ -17,16 +29,19 @@ public class Pion_blanc extends Piece {
 			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-1) == false) {return false;}
 			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-2) == false) {return false;}
 		}
-		if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {
+		else if (depart.getX() == arrive.getX() && arrive.getY() == depart.getY()-1) {
 			if (this.echiquier.verif_case_vide(arrive.getX(), depart.getY()-1) == false) {return false;}
 		}
+		else if ((depart.getX()-1 == arrive.getX() && arrive.getY()-1 == depart.getY())
+			|| (depart.getX()+1 == arrive.getX() && arrive.getY()-1 == depart.getY())
+			|| (depart.getX()-1 == arrive.getX() && arrive.getY()+1 == depart.getY())
+			|| (depart.getX()+1 == arrive.getX() && arrive.getY()+1 == depart.getY())) {
+			if (this.echiquier.verif_case_vide(arrive.getX(), arrive.getY()) == false){
+				this.premierCoup = false;
+				return true;}
+			else {return false;}
+			}
+		this.premierCoup = false;
 		return true;
 	}
-		
-
-	public boolean premierCoup() {
-		if ( this.echiquier.premierCoup() == true ) {return true;}
-		else {return false;}
-	}
-	
 }
