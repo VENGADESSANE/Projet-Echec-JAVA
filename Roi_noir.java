ed8a0a1b2c3cd2705a2f63121be0db725ab8a3a0
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-27 22:16:20 +0200

    Update Roi_noir.java

--- Roi_noir.java
+++ Roi_noir.java
@@ -1,41 +1,39 @@
 public class Roi_noir extends Piece {
+	private Echiquier echiquier;
 
-	public Roi_noir() {
+	public Roi_noir(Echiquier echiquier) {
 		super("♚",1);
+		this.echiquier = echiquier;
 	}
 	
 	//Mouvement une case devant 
 	public boolean devant(Case depart, Case arrive) {
-		if(depart.devant(arrive)) {
-			if(arrive.getX()== depart.getX()-1)
-				return true;
+		if(depart.getX() == arrive.getX() && depart.getY()+1 == arrive.getY()) {
+			return true;
 		}
 		return false;
 	}
 	
 	//Mouvement une case derrière
 	public boolean derriere(Case depart, Case arrive) {
-		if(depart.derriere(arrive)) {
-			if(arrive.getX() == depart.getX()+1)
-				return true;
+		if(depart.getX() == arrive.getX() && depart.getY()-1 == arrive.getY()) {
+			return true;
 		}
 		return false;
 	}
 	
 	//Mouvement une case à droite
 	public boolean cote_droit(Case depart, Case arrive) {
-		if(depart.cote_droit(arrive)) {
-			if(arrive.getY() == depart.getY()+1)
-				return true;
+		if(depart.getX()+1 == arrive.getX() && depart.getY() == arrive.getY()) {
+			return true;
 		}
 		return false;
 	}
 	
 	//Mouvement une case à gauche
 	public boolean cote_gauche(Case depart, Case arrive) {
-		if(depart.cote_gauche(arrive)) {
-			if(arrive.getY() == depart.getY()-1)
-				return true;
+		if(depart.getX()-1 == arrive.getX() && depart.getY() == arrive.getY()) {
+			return true;
 		}
 		return false;
 	}
@@ -50,7 +48,7 @@ public class Roi_noir extends Piece {
 	
 	//Mouvement en bas à gauche
 	public boolean gauche_bas(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX()+1 && arrive.getY() == depart.getY()-1)
+		if(arrive.getX() == depart.getX()-1 && arrive.getY() == depart.getY()+1)
 			return true;
 		else
 			return false;
@@ -58,7 +56,7 @@ public class Roi_noir extends Piece {
 	
 	//Mouvement en haut à droite
 	public boolean droite_haut(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX()-1 && arrive.getY() == depart.getY()+1)
+		if(arrive.getX() == depart.getX()+1 && arrive.getY() == depart.getY()-1)
 			return true;
 		else 
 			return false;
@@ -102,6 +100,6 @@ public class Roi_noir extends Piece {
 		else 
 			return false;
 	}
+	public boolean verif_case_libre(Case depart, Case arrive) {return true;}
 	
 }
-
