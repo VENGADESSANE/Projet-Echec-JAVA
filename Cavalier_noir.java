9adf7550e66f3032a1f3e42fa2a93ac687a069ce
VENGADESSANE <81411427+VENGADESSANE@users.noreply.github.com>
2021-04-27 22:13:01 +0200

    Update Cavalier_noir.java

--- Cavalier_noir.java
+++ Cavalier_noir.java
@@ -1,92 +1,31 @@
 public class Cavalier_noir extends Piece {
+	private Echiquier echiquier;
 
-	public Cavalier_noir() {
+	public Cavalier_noir(Echiquier echiquier) {
 		super("♞",1);
+		this.echiquier = echiquier;
 	}
 	
 	public boolean verif(Case depart, Case arrive) {
-		if(this.devant_droite(depart, arrive))
-			return true; 
-		else if(this.devant_gauche(depart, arrive)) {
-			return true;
-		}
-		else if(this.droite_haut(depart, arrive)) {
-			return true;
-		}
-		else if(this.droite_bas(depart, arrive)) {
-			return true;
-		}
-		else if(this.gauche_haut(depart, arrive)) {
-			return true;
-		}
-		else if(this.gauche_bas(depart, arrive)) {
-			return true;
-		}
-		else if(this.derriere_droite(depart, arrive)) {
-			return true;
-		}
-		else if(this.derriere_gauche(depart, arrive)) {
-			return true;
-		}
+		if(depart.getY()+2 == arrive.getY() && depart.getX()+1 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()-2 == arrive.getY() && depart.getX()+1 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()+2 == arrive.getY() && depart.getX()-1 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()-2 == arrive.getY() && depart.getX()-1 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()-1 == arrive.getY() && depart.getX()+2 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()-1 == arrive.getY() && depart.getX()-2 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()+1 == arrive.getY() && depart.getX()+2 == arrive.getX()) {
+			return true;}
+		else if(depart.getY()+1 == arrive.getY() && depart.getX()-2 == arrive.getX()) {
+			return true;}
 		else
 			return false;
 		}
-	
-	public boolean devant_droite(Case depart, Case arrive) {
-			if(arrive.getX() == depart.getX()- 1 && arrive.getY() == depart.getY() - 2)
-				return true;
-			else 
-				return false;
-	}
-	
-	public boolean devant_gauche(Case depart, Case arrive) {
-			if(arrive.getX() == depart.getX() + 1 && arrive.getY() == depart.getY() - 2)
-				return true;
-			else 
-				return false;
-	}
-	
-	public boolean droite_haut(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX() - 2 && arrive.getY() == depart.getY() - 1)
-			return true;
-		else 
-			return false;
-	}
-	
-	public boolean droite_bas(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX() - 2 && arrive.getY() == depart.getY() + 1)
-			return true;
-		else 
-			return false;
-	}
-	
-	public boolean gauche_haut(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX() + 2 && arrive.getY() == depart.getY() - 1)
-			return true;
-		else 
-			return false;
-	}
-	
-	public boolean gauche_bas(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX() + 2 && arrive.getY() == depart.getY() + 1)
-			return true;
-		else 
-			return false;
-	}
-	
-	public boolean derriere_droite(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX()- 1 && arrive.getY() == depart.getY() + 2)
-			return true;
-		else 
-			return false;
-	}
-	
-	public boolean derriere_gauche(Case depart, Case arrive) {
-		if(arrive.getX() == depart.getX()+ 1 && arrive.getY() == depart.getY() + 2)
-			return true;
-		else 
-			return false;
-	}
-	
+	public boolean verif_case_libre(Case depart, Case arrive) {
+		return true;}
 }
-
