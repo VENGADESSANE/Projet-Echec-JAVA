efee76ffa0a6f004c678dd05c990316625641138
satia05 <62488137+satia05@users.noreply.github.com>
2021-05-15 11:43:03 +0200

    Update Cavalier_blanc.java

--- Cavalier_blanc.java
+++ Cavalier_blanc.java
@@ -4,6 +4,7 @@ public class Cavalier_blanc extends Piece {
 		super("♘",0,echiquier);
 	}
 	
+	//la méthode vérifie tout les déplacements possible de la pièce cavalier 
 	public boolean verif(Case depart, Case arrive) {
 		if(depart.getY()+2 == arrive.getY() && depart.getX()+1 == arrive.getX()) {
 			return true;}
