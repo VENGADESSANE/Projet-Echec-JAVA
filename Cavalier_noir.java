a59aa025aba48236e48c3f41b95b911a70e147b2
satia05 <62488137+satia05@users.noreply.github.com>
2021-05-15 11:43:16 +0200

    Update Cavalier_noir.java

--- Cavalier_noir.java
+++ Cavalier_noir.java
@@ -4,6 +4,7 @@ public class Cavalier_noir extends Piece {
 		super("♞",1,echiquier);
 	}
 	
+	//la méthode vérifie tout les déplacements possible de la pièce cavalier 
 	public boolean verif(Case depart, Case arrive) {
 		if(depart.getY()+2 == arrive.getY() && depart.getX()+1 == arrive.getX()) {
 			return true;}
